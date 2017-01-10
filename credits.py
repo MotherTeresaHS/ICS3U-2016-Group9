1c690fee16db8c9a6c6231d2c50427273eb71a68
MargaretVenes <margaret.venes@ocsbstudent.ca>
2017-01-10 13:24:03 -0500

    Update credits.py

--- credits.py
+++ credits.py
@@ -1,4 +1,6 @@
 import ui
-
+menu_view = ui.load_view("main_menu_scene.pyui")
+def back_touch_up_inside(sender):
+	menu_view.present("fullscreen")
 view = ui.load_view()
 view.present('full_screen')
